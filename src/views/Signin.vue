356c30055fd92e7439d1814cd1719067cd3dcad7
shiu <shiyu.wang@wengegroup.com>
2022-11-17 19:42:47 +0800

    fix: add signin page

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,9 +1,61 @@
-<script setup lang="ts">
+<script lang="ts" setup>
+import { reactive, ref } from "@vue/reactivity";
+import { FormInstance, FormRules } from "element-plus";
+
+const form = reactive({
+  username: '',
+  password: '',
+})
+const formRef = ref<FormInstance>()
+
+const rules = reactive<FormRules>({
+  username: [
+    { required: true, message: 'Please enter your username', trigger: 'blur' },
+    { min: 5, max: 10, message: 'Your username length should be 5 to 10', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: 'Please enter your password', trigger: 'blur' },
+  ],
+
+})
+const confirm = () => {
+  formRef.value?.validate((valid) => {
+    if (valid) {
+      // TODO: signin
+    } else {
+      console.log('invalid input')
+    }
+  })
+}
+
 </script>
 <template>
-  <div>signin</div>
-</template>
+  <el-row>
+    <el-col :md="6" :push="9">
+      <el-card class="mt-16">
+        <template #header>
+          <h3>Sign in</h3>
+        </template>
 
+        <el-form ref="formRef" label-width="120px" :model="form" :rules="rules">
+          <el-form-item label="Username" prop="username">
+            <el-input v-model="form.username" placeholder="Username"></el-input>
+          </el-form-item>
+          <el-form-item label="Password" prop="password">
+            <el-input v-model="form.password" placeholder="Password"></el-input>
+          </el-form-item>
+        </el-form>
+        <div class="ml-4">
+          <el-button type="primary" @click="confirm" class="mt-2">Confirm</el-button>
+          <el-link type="primary" href="#/signin" :underline="true" class="ml-4 pt-2">sign up</el-link>
+          <el-link type="primary" href="#/forget" :underline="true" class="ml-4 pt-2">forget password?</el-link>
+
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
+
+</template>
 <style lang="scss" scoped>
 
 </style>
